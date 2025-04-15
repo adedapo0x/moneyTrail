7d70526c125848e612ebd7412cd807b7a74aed0a
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-15 16:16:16 +0100

    created registerUser DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
+    @Post()
+    register(@Body() dto){
+
+    }
+
     @Post()
     login(){
         "hello sir"
